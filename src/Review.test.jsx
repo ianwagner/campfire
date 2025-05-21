1e39afd7be196140efc1d96ceaa09630c195d75e
wags <i.doyle.w@gmail.com>
2025-05-20 18:48:42 -0700

    feat: load review ads from ad groups

--- src/Review.test.jsx
+++ src/Review.test.jsx
@@ -22,18 +22,27 @@ afterEach(() => {
 
 test('loads ads from subcollections', async () => {
   const batchSnapshot = {
-    docs: [
-      { id: 'batch1', data: () => ({ brandCode: 'BR1' }) },
-    ],
+    docs: [{ id: 'batch1', data: () => ({ brandCode: 'BR1' }) }],
   };
   const adsSnapshot = {
-    docs: [
-      { id: 'ad1', data: () => ({ adUrl: 'url1' }) },
-    ],
+    docs: [{ id: 'ad1', data: () => ({ adUrl: 'url1' }) }],
   };
-  getDocs.mockResolvedValueOnce(batchSnapshot).mockResolvedValueOnce(adsSnapshot);
+  const groupSnapshot = {
+    docs: [{ id: 'group1', data: () => ({ brandCode: 'BR1' }) }],
+  };
+  const assetSnapshot = {
+    docs: [{ id: 'asset1', data: () => ({ firebaseUrl: 'url2' }) }],
+  };
+
+  getDocs
+    .mockResolvedValueOnce(batchSnapshot)
+    .mockResolvedValueOnce(adsSnapshot)
+    .mockResolvedValueOnce(groupSnapshot)
+    .mockResolvedValueOnce(assetSnapshot);
 
   render(<Review user={{ uid: 'u1' }} brandCodes={['BR1']} />);
 
-  await waitFor(() => expect(screen.getByRole('img')).toHaveAttribute('src', 'url1'));
+  await waitFor(() =>
+    expect(screen.getByRole('img')).toHaveAttribute('src', 'url1')
+  );
 });
