47af123f2addb3763920cd22999b896b64ba8708
wags <i.doyle.w@gmail.com>
2025-05-25 13:11:01 -0700

    fix: show real signup stepper

--- src/SignUpStepper.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SignUpStepper = () => (
-  <div className="flex justify-center items-center min-h-screen">
-    Sign Up Stepper Placeholder
-  </div>
-);
-
-export default SignUpStepper;
