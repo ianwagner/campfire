cb0066c9a50b64647a3aa347fc42d9a0acebf4c6
wags <i.doyle.w@gmail.com>
2025-05-26 17:38:58 -0700

    feat: add design tokens and use css variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,30 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     fontFamily: {
-      sans: ['Rubik', 'sans-serif'],
+      sans: ['var(--font-primary)'],
     },
-    extend: {},
+    extend: {
+      fontSize: {
+        base: 'var(--font-size-base)',
+        lg: 'var(--font-size-lg)',
+      },
+      fontWeight: {
+        normal: 'var(--font-weight-normal)',
+        semibold: 'var(--font-weight-semibold)',
+        bold: 'var(--font-weight-bold)',
+      },
+      spacing: {
+        sm: 'var(--spacing-sm)',
+        md: 'var(--spacing-md)',
+        lg: 'var(--spacing-lg)',
+      },
+      borderRadius: {
+        base: 'var(--border-radius-base)',
+      },
+      boxShadow: {
+        base: 'var(--box-shadow-base)',
+      },
+    },
   },
   plugins: [],
 };
