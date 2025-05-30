51b000060530c55f7839be3f412bd17edf466e2b
wags <i.doyle.w@gmail.com>
2025-05-30 08:45:35 -0700

    Update default logo URL

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const DEFAULT_LOGO_URL = 'https://firebasestorage.googleapis.com/v0/b/tak-campfire-main/o/StudioTak%2Flogo_new.webp?alt=media&token=1f08d552-6c85-444d-ac4f-1e895e97e5bd';
+export const DEFAULT_LOGO_URL =
+  'https://firebasestorage.googleapis.com/v0/b/tak-campfire-main/o/Campfire%2Fsite-logo%2Flogo_new.webp?alt=media&token=5d7b6b20-9979-4d66-a098-97732573a7a2';
