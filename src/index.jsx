84c0a4b8ee24d6b8546d61fb6a0c2205f076dac9
wags <i.doyle.w@gmail.com>
2025-05-31 19:44:23 -0700

    Remove StrictMode to prevent double mount

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,7 @@ import App from './App';
 import './global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 if (import.meta.env.DEV) {
   const warnIfBase64Bg = (el) => {
