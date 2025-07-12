c1c05b6d3501a7de99fdbd2cc46144d9b495cc31
wags <i.doyle.w@gmail.com>
2025-07-12 10:47:48 -0700

    Update admin dashboard to monthly recipe counts

--- src/components/DateRangeSelector.jsx
+++ src/components/DateRangeSelector.jsx
@@ -4,14 +4,14 @@ const DateRangeSelector = ({ startDate, endDate, onChange }) => {
   return (
     <div className="flex items-center space-x-2 mb-4">
       <input
-        type="date"
+        type="month"
         value={startDate}
         onChange={(e) => onChange({ start: e.target.value, end: endDate })}
         className="border px-2 py-1 rounded"
       />
       <span>-</span>
       <input
-        type="date"
+        type="month"
         value={endDate}
         onChange={(e) => onChange({ start: startDate, end: e.target.value })}
         className="border px-2 py-1 rounded"
