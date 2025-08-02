87e503a1ce029f225d546363b789a15e858a8b17
wags <i.doyle.w@gmail.com>
2025-08-02 11:03:22 -0700

    Use fixed table layout with ellipsis for admin tables

--- src/components/common/Table.jsx
+++ src/components/common/Table.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Table = ({ children }) => (
   <div className="overflow-x-auto table-container">
-    <table className="ad-table min-w-max text-sm">{children}</table>
+    <table className="ad-table w-full table-fixed text-sm">{children}</table>
   </div>
 );
 
