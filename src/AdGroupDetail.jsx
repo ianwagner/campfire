8c7ea7ef9e932d11040de737e5af06f8ea69624f
wags <i.doyle.w@gmail.com>
2025-08-03 14:38:59 -0700

    Allow saving recipes via ad group details

--- src/AdGroupDetail.jsx
+++ src/AdGroupDetail.jsx
@@ -2797,6 +2797,7 @@ const AdGroupDetail = () => {
             onSave={saveRecipes}
             brandCode={group?.brandCode}
             hideBrandSelect
+            showOnlyResults
             externalOnly
           />
         </Modal>
