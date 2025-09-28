18919ba1ab935f66e2a8e39b30575168fc9dd999
wags <i.doyle.w@gmail.com>
2025-09-27 17:08:46 -0700

    Prevent scroll wheel from altering data filters

--- src/ClientData.test.jsx
+++ src/ClientData.test.jsx
@@ -73,3 +73,56 @@ test('saves edited values to metadata', async () => {
     expect(updateDoc).toHaveBeenCalledWith('docRef', { 'metadata.url': 'new' })
   );
 });
+
+test('scrolling the filters delegates to window scrolling', async () => {
+  getDocs
+    .mockResolvedValueOnce({
+      docs: [
+        {
+          id: 'g1',
+          data: () => ({
+            month: '2023-09',
+            dueDate: new Date('2023-09-15'),
+            brandCode: 'BR1',
+          }),
+        },
+      ],
+    })
+    .mockResolvedValueOnce({
+      docs: [
+        {
+          id: 'g1',
+          data: () => ({ name: 'Group1', brandCode: 'BR1', metadata: {} }),
+        },
+      ],
+    })
+    .mockResolvedValueOnce({
+      docs: [{ data: () => ({ storeId: '' }) }],
+    })
+    .mockResolvedValueOnce({
+      docs: [
+        {
+          id: 'r1',
+          data: () => ({ metadata: { url: 'old' }, components: {} }),
+        },
+      ],
+    })
+    .mockResolvedValueOnce({ docs: [] })
+    .mockResolvedValueOnce({ docs: [] });
+
+  render(<ClientData brandCodes={['BR1']} />);
+
+  const selects = await screen.findAllByRole('combobox');
+  fireEvent.change(selects[0], { target: { value: '2023-09' } });
+  fireEvent.change(selects[2], { target: { value: 'BR1' } });
+
+  await screen.findByText('Group1');
+
+  const scrollBySpy = jest.spyOn(window, 'scrollBy').mockImplementation(() => {});
+
+  fireEvent.wheel(selects[2], { deltaY: 120, deltaX: 0 });
+
+  expect(scrollBySpy).toHaveBeenCalledWith({ top: 120, left: 0, behavior: 'auto' });
+
+  scrollBySpy.mockRestore();
+});
