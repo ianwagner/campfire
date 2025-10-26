e201d7648f8e210dcaf2297e7f1b4e267bfb88bb
wags <i.doyle.w@gmail.com>
2025-10-26 14:47:39 -0700

    Ensure storage URLs append alt=media for public reviews

--- src/utils/sanitizeSrc.test.js
+++ src/utils/sanitizeSrc.test.js
@@ -6,8 +6,18 @@ describe('sanitizeSrc', () => {
     expect(sanitizeSrc(url)).toBe(`${url}?alt=media`);
   });
 
+  test('appends alt=media to storage.googleapis.com URLs', () => {
+    const url = 'https://storage.googleapis.com/custom-bucket/path/to/ad.png';
+    expect(sanitizeSrc(url)).toBe(`${url}?alt=media`);
+  });
+
   test('does not modify URLs that already include alt=media', () => {
     const url = 'https://firebasestorage.googleapis.com/v0/b/foo/o/bar.png?alt=media';
     expect(sanitizeSrc(url)).toBe(url);
+  });
+
+  test('preserves existing queries on storage.googleapis.com URLs', () => {
+    const url = 'https://storage.googleapis.com/custom-bucket/path/ad.png?token=abc';
+    expect(sanitizeSrc(url)).toBe(`${url}&alt=media`);
   });
 });
